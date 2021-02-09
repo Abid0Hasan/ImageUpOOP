d17aaeb0709b68413e2b5de56cd961223bc1c39a
Abid Ibn Habib <34665203+Abid0Hasan@users.noreply.github.com>
2021-02-09 15:27:21 +0600

    Update index.php

--- project/index.php
+++ project/index.php
@@ -12,40 +12,39 @@ include 'lib/Database.php';
 
  <div class="myform">
  <?php
- //Ready image for upload
- if($_SERVER["REQUEST_METHOD"] == "POST"){
-  $permited = array('jpg','jpeg','png','gif');
-  $file_name = $_FILES['image']['name']; 
-  $file_size = $_FILES['image']['size']; 
-  $file_temp = $_FILES['image']['tmp_name'];
-  
-  //With Unique Name
-  $div = explode('.', $file_name);
-  $file_ext = strtolower(end($div));
-  $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
-  $uploaded_image ="uploads/".$unique_image;
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $permited  = array('jpg', 'jpeg', 'png', 'gif');
+    $file_name = $_FILES['image']['name'];
+    $file_size = $_FILES['image']['size'];
+    $file_temp = $_FILES['image']['tmp_name'];
 
-  //Image validation
-  if(empty($file_name)){
-      echo "<span class='error'>Please Select any Image !</span>";
-    }elseif($file_size > 1048567){
-      echo "<span class='error'>Image Size should be less then 1MB!</span>";
-    }elseif(in_array($file_ext, $permitted) === false){
-      echo "<span class='error'>You can upload only:-".implode(', ', $permited)."</span>";
-    }else{
-     //upload image to database and folder
-        move_uploaded_file($file_temp, $uploaded_image);
-        $query = "INSERT INTO tbl_image(image) VALUES('$uploaded_image')"; 
-        $inserted_row = $db->insert($query);
-        if($inserted_row){
-            echo "<span class='success'>Image Inserted Successfully.</span>";
-        }else{
-            echo "<span class='error'>Image Not Inserted !</span>"; 
-        } 
+    $div = explode('.', $file_name);
+    $file_ext = strtolower(end($div));
+    $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+    $uploaded_image = "uploads/".$unique_image;
+
+    if (empty($file_name)) {
+     echo "<span class='error'>Please Select any Image !</span>";
+    }elseif ($file_size >1048567) {
+     echo "<span class='error'>Image Size should be less then 1MB!
+     </span>";
+    } elseif (in_array($file_ext, $permited) === false) {
+     echo "<span class='error'>You can upload only:-"
+     .implode(', ', $permited)."</span>";
+    } else{
+    move_uploaded_file($file_temp, $uploaded_image);
+    $query = "INSERT INTO tbl_image(image) 
+    VALUES('$uploaded_image')";
+    $inserted_rows = $db->insert($query);
+    if ($inserted_rows) {
+     echo "<span class='success'>Image Inserted Successfully.
+     </span>";
+    }else {
+     echo "<span class='error'>Image Not Inserted !</span>";
     }
-}
- ?>
-
+    }
+   }
+  ?>
 
 
   <form action="" method="post" enctype="multipart/form-data">
