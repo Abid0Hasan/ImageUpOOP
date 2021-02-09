240a91612d5b594751d8f827eded0cbe25352325
Abid Ibn Habib <34665203+Abid0Hasan@users.noreply.github.com>
2021-02-09 15:21:49 +0600

    PHP Image Up using OOP.

---
+++ project/fileupload.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 09, 2021 at 10:17 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fileupload`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_image`
+--
+
+CREATE TABLE `tbl_image` (
+  `id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_image`
+--
+
+INSERT INTO `tbl_image` (`id`, `image`) VALUES
+(22, 'uploads/8a89765428.jpg'),
+(23, 'uploads/f6c2d56c71.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_image`
+--
+ALTER TABLE `tbl_image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_image`
+--
+ALTER TABLE `tbl_image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ project/index.php
@@ -0,0 +1,122 @@
+<?php include 'inc/header.php';?>
+
+
+<?php
+include 'lib/config.php';
+include 'lib/Database.php';
+
+  $db = new Database();
+
+?>
+
+
+ <div class="myform">
+ <?php
+ //Ready image for upload
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $permitted = array('jpg','jpeg','png','gif');
+  $file_name = $_FILES['image']['name']; 
+  $file_size = $_FILES['image']['size']; 
+  $file_temp = $_FILES['image']['tmp_name'];
+  
+  //With Unique Name
+  $div = explode('.', $file_name);
+  $file_ext = strtolower(end($div));
+  $unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+  $uploaded_image ="uploads/".$unique_image;
+
+  //Image validation
+  if(empty($file_name)){
+      echo "<span class='error'>Please Select any Image !</span>";
+    }elseif($file_size > 1048567){
+      echo "<span class='error'>Image Size should be less then 1MB!</span>";
+    }elseif(in_array($file_ext, $permitted) === false){
+      echo "<span class='error'>You can upload only:-".implode(', ', $permited)."</span>";
+    }else{
+     //upload image to database and folder
+        move_uploaded_file($file_temp, $uploaded_image);
+        $query = "INSERT INTO tbl_image(image) VALUES('$uploaded_image')"; 
+        $inserted_row = $db->insert($query);
+        if($inserted_row){
+            echo "<span class='success'>Image Inserted Successfully.</span>";
+        }else{
+            echo "<span class='error'>Image Not Inserted !</span>"; 
+        } 
+    }
+}
+ ?>
+
+
+
+  <form action="" method="post" enctype="multipart/form-data">
+   <table>
+    <tr>
+     <td>Select Image</td>
+     <td><input type="file" name="image"/></td>
+    </tr>
+    <tr>
+     <td></td>
+     <td><input type="submit" name="submit" value="Upload"/></td>
+    </tr>
+   </table>
+  </form>
+  
+  <table width="100%">
+  <tr>
+  <th width="30%">No.</th>
+  <th width="40%">Image</th>
+  <th width="30%">Action</th>
+  </tr>
+
+   
+   <?php
+   // Delete Functionality
+
+      if(isset($_GET['del'])){
+          $id = $_GET['del'];
+
+       //Delete from Folder / unlink image
+       $getQuery = "SELECT * FROM tbl_image WHERE id='$id'";
+       $getImg = $db->select($getQuery);
+       if($getImg){
+         while($imgData = $getImg->fetch_assoc()){
+           $delImg = $imgData['image'];
+           unlink($delImg);
+         }
+       }
+      
+       //Delete From Database
+        $query = "DELETE FROM tbl_image WHERE id='$id'";
+        $delImage = $db->delete($query);
+        if($delImage){
+          echo "<span class='success'>Image Deleted Successfully.</span>";
+        }else{
+          echo "<span class='error'>Image Not Deleted !</span>";
+        }
+   
+  }
+   
+   ?>
+
+
+  <?php
+  //Display Image functionality.
+  $query = "SELECT * FROM tbl_image";
+  $getImage = $db->select($query);
+  if($getImage){
+      $i = 0;
+      while($result = $getImage->fetch_assoc()){
+      $i++;
+  ?>
+ 
+  <tr>
+    <td><?php echo $i; ?></td>
+    <td><img src="<?php echo $result['image']; ?>" height="40px" 
+      width="50px"/></td>
+    <td><a href="?del=<?php echo $result['id']; ?>">Delete</a></td>
+   </tr>
+   <?php } }?>
+   </table>
+  
+ </div>
+<?php include 'inc/footer.php';?>
